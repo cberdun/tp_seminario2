4522abc2a58aa7fea76a8113c40104b64aba7d93
cberdun <112456257+cberdun@users.noreply.github.com>
2023-11-16 16:22:12 -0300

    Update control-env.sh

--- control-env.sh
+++ control-env.sh
@@ -5,20 +5,31 @@ function stop {
   docker-compose --project-name wksp down
 }
 
-function cleanup {
-  echo "Removing volume"
-  docker volume rm wksp_postgres-data
-}
-
 function start {
   echo "Starting up"
   docker-compose --project-name wksp up -d
 }
 
+function update {
+  echo "Updating code ..."
+  git pull --all
+
+  echo "Updating docker images ..."
+  docker-compose --project-name wksp pull
+
+  echo "You probably should restart"
+}
+
 function info {
   echo '
-  Everything is ready, access http://localhost/ to learn more
+  Everything is ready, access your host to learn more (ie: http://localhost/)
   '
+}
+
+function token {
+  echo 'Your TOKEN for Jupyter Notebook is:'
+  SERVER=$(docker exec -it jupyter jupyter notebook list)
+  echo "${SERVER}" | grep '/notebook' | sed -E 's/^.*=([a-z0-9]+).*$/\1/'
 }
 
 case $1 in
@@ -36,12 +47,20 @@ case $1 in
   cleanup
     ;;
 
+  update )
+  update
+    ;;
+
   logs )
   docker-compose --project-name wksp logs -f
     ;;
 
+  token )
+  token
+   ;;
+   
   * )
-  printf "ERROR: Missing command\n  Usage: `basename $0` (start|stop|cleanup|logs)\n"
+  printf "ERROR: Missing command\n  Usage: `basename $0` (start|stop|cleanup|token|logs|update|superset-start|superset-stop|superset-init)\n"
   exit 1
     ;;
 esac
